f09b198e45a876a84f423cfc698119a482d52926
Mauro <mauro_carlos14@hotmail.com>
2022-08-24 09:28:44 -0400

    comando Javascript

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,15 @@
-let nome;
-let email;
-let profissao;
-let idade;
-let pais;
+let nome
+let email
+let profissao
+let idade
+let pais
+nome = "mauro carlos"
+email = "mauro_carlos14@hotmail.com"
+profissao = "tecnico"
+idade = 55
+pais = "tailandia"
+console.log (nome)
+console.log (email)
+console.log (profissao)
+console.log (idade)
+console.log(pais)
