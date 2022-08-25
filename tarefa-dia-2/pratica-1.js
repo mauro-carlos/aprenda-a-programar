04330b7c51bd79b0a92008df13e6493c1f01f21f
Mauro <mauro_carlos14@hotmail.com>
2022-08-25 09:10:25 -0400

    template string

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,13 +3,19 @@ let email
 let profissao
 let idade
 let pais
-nome = "mauro carlos"
+nome = "Mauro Carlos"
 email = "mauro_carlos14@hotmail.com"
-profissao = "tecnico"
+profissao = "Tecnico em Telecomunicações"
 idade = 55
-pais = "tailandia"
+pais = "Tailandia"
 console.log (nome)
 console.log (email)
 console.log (profissao)
 console.log (idade)
 console.log(pais)
+
+let mensagem  = `Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos.Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${pais}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! D:`
+
+
+ 
+ console.log(mensagem)
