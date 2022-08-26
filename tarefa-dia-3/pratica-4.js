0f6c568721d31a9b23f828d98573ce64fb093fdb
Mauro <mauro_carlos14@hotmail.com>
2022-08-26 09:23:36 -0400

    numeros primos usando While

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,13 @@
+let numero =7
+let divisor
+divisor=2
+while (numero%divisor !=0){
+    divisor++
+}
+if (divisor==numero){
+    console.log(`o numero ${numero} É Primo`)
+}
+  else {
+    console.log(`o numero ${numero} NÃO É Primo`)
+  }
+
